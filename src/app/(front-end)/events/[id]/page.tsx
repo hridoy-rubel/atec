2af2cba5b0a9813755a55fef64e0ffc182157756
Md-Hazrat <mdhazratali8866@gmail.com>
2024-05-20 20:05:11 +0600

    working on event details page

--- src/app/(front-end)/events/[id]/page.tsx
+++ src/app/(front-end)/events/[id]/page.tsx
@@ -1,29 +1,18 @@
 import { FaLocationDot } from "react-icons/fa6";
-import EventContactCard from "@/components/shared/event-contact-card";
 import EventTimer from "@/components/shared/event-timer";
 import Image from "next/image";
-import EventJourney from "@/components/shared/event-journey";
-import React from 'react';
-
+import React from "react";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import SubHeading from "@/components/shared/sub-heading";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const EventDetails = () => {
-  const containerStyle = {
-    width: '400px',
-    height: '400px'
-  };
-  
-  const center = {
-    lat: -34.397,
-    lng: 150.644
-  };
-
   return (
     <section
       id="about-section"
       aria-label="about us section"
       className="w-full mb-10"
     >
-       
       <div className="w-full ">
         <Image
           src="/images/about.jpg"
@@ -33,28 +22,88 @@ const EventDetails = () => {
           className="h-screen w-full"
         />
       </div>
-      <div className="mt-8">
-      <EventJourney />
-      </div>
-     
-      <EventTimer />
-      <div className="max-w-lg mx-auto bg-white shadow-md rounded-lg sm:bg-white text-black overflow-hidden m-8 mr-8 ">
-        <div className="p-4">
-          <h2 className="font-bold mb-2">Location</h2>
-          <div className="flex items-center justify-center">
-            <FaLocationDot className="size-8 mb-10" />
-            <p className="ml-2">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque
-              suscipit iure vitae distinctio et tempore perferendis sapiente
-              dolore quae eligendi?
+
+
+
+      <div className="container ">
+        <div className="flex items-center">
+          <div className="flex items-center  shadow rounded-full h-16 w-16 bg-teal-400 m-2">
+            <p>09:30AM</p>
+          </div>
+          <div>
+            <ArrowRightIcon />
+          </div>
+          <div className="overflow-hidden shadow-lg mt-6 gap-2 rounded">
+            <h1 className="text-center">Event Start</h1>
+            <p className="m-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi
+              inventore officia veritatis accusantium nam ipsa cum accusamus?
+              Nemo, explicabo consequuntur possimus, enim quia et, omnis minus
+              ratione minima atque iusto.
             </p>
           </div>
         </div>
       </div>
-      <EventContactCard /> 
-     
 
+      <EventTimer />
+
+      <div className="rounded overflow-hidden shadow-lg mt-6">
+        <div className="max-w-lg mx-auto shadow-md rounded-lg text-black overflow-hidden m-8 mr-8 px-6">
+          <div className="flex justify-center mt-2 ">
+            <SubHeading content="Contact Person" />
+          </div>
+          <div className="container flex max-w-lg mx-auto shadow-md bg-gradient-to-r from-green-400 to-blue-500 rounded-lg sm:bg-slate-300 text-black overflow-hidden m-8">
+            <div className="mt-8">
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="m-4">
+              <h1>Name: Md Hazrat Ali</h1>
+              <h1>Designation: execative director</h1>
+              <h1>Phone Number: 018******66</h1>
+            </div>
+          </div>
+
+          <div className="container flex max-w-lg mx-auto bg-gradient-to-r from-pink-600 to-green-500 shadow-md rounded-lg sm:bg-slate-300 text-black overflow-hidden m-8">
+            <div className="mt-8">
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="m-4">
+              <h1>Name:</h1>
+              <h1>Designation:</h1>
+              <h1>Phone Number:</h1>
+            </div>
+          </div>
+
+          <div className="container flex max-w-lg mx-auto bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-md rounded-lg sm:bg-slate-300 text-black overflow-hidden m-8">
+            <div className="mt-8">
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="m-4">
+              <h1>Name:</h1>
+              <h1>Designation:</h1>
+              <h1>Phone Number:</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/shared/event-timer.tsx
+++ src/components/shared/event-timer.tsx
@@ -52,79 +52,201 @@ const EventTimer = () => {
 
   return (
     <section id="event-section" aria-label="event section" className="w-full">
-      <div className="max-w-lg mx-auto bg-white shadow-md rounded-lg overflow-hidden m-8 mr-8 font-bold">
-        <div className="p-4">
-          <div className="flex items-center justify-between ">
-            <div className="text-sm text-gray-600 font-bold">
-              <h3>Start Time</h3>
-              {moment(eventStartTime).format("h:mm A")}
-            </div>
+      <div className="rounded overflow-hidden shadow-lg mt-10 ">
+        <div className="container flex items-center justify-between ">
+          <div className="text-sm text-gray-600 font-bold">
+            <h3>Start Time</h3>
+            {moment(eventStartTime).format("h:mm A")}
+          </div>
 
-            <div className="mb-4 ">
-              <h3 className="text-blue-700 text-center text-xl ">June</h3>
-              <h1 className="text-blue-700 font-bold text-xl">03</h1>
-              <p className="text-blue-700 text-center text-xl">Friday</p>
+          <div className="mb-4 ">
+            <h3 className="text-blue-700 text-center text-xl ">June</h3>
+            <h1 className="text-red-600 font-bold text-xl">03</h1>
+            <p className="text-blue-700 text-center text-xl">Friday</p>
+          </div>
+          <div className="text-red-600 text-center font-bold">
+            <h3> End Time</h3>
+            {moment(eventEndTime).format("h:mm A")}
+          </div>
+        </div>
+
+        <div className="flex text-lg p-2 rounded-lg z-10">
+          <div className=" shadow rounded-lg overflow-hidden m-8 mr-8 font-bold p-3">
+            <div className="">
+              <span className="time">{Math.floor(days / 10)}</span>
+              <span className="mr-3"> </span>
+              <span className="text-lg">{days % 10}</span>
             </div>
-            <div className="text-red-600 text-center font-bold">
-              <h3> End Time</h3>
-              {moment(eventEndTime).format("h:mm A")}
+            <div className="timer-label">
+              <span className=" text-center">Days</span>
             </div>
           </div>
 
-          <div className="timer-wrapper bg-gradient-to-rbg-gradient-to-r from-green-400 to-blue-500">
-            <div className="timer-inner">
-              <div className="timer-wrapper bg-gradient-to-r from-green-400 to-blue-500">
-                <div className="timer-row">
-                  <h1 className="timer-segment h-8 w-20">
-                    <span className="time">{Math.floor(hours / 10)}</span>
-                  </h1>
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{days % 10}</span>
-                    <span className="label p-4 mr-8 text-red">Days</span>
-                  </h1>
-                </div>
-
-                <div className="timer-row">
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{Math.floor(hours / 10)}</span>
-                  </h1>
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{hours % 10}</span>
-                    <span className="label p-4 mr-8">Hours</span>
-                  </h1>
-                </div>
-
-                <div className="timer-row">
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{Math.floor(minute / 10)}</span>
-                  </h1>
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{minute % 10}</span>
-                    <span className="label p-4 mr-8">Mins</span>
-                  </h1>
-                </div>
-
-                <div className="timer-row">
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{Math.floor(second / 10)}</span>
-                  </h1>
-                  <h1 className="timer-segment h-8 w-20 ">
-                    <span className="time">{second % 10}</span>
-                    <span className="label p-4 mr-8">Sec</span>
-                  </h1>
-                </div>
-              </div>
+          <div className="timer-card max-w-lg mx-auto shadow-md rounded-lg overflow-hidden m-8 mr-8 font-bold p-2">
+            <div className="timer-value">
+              <span className="text-lg">{Math.floor(hours / 10)}</span>
+              <span className="mr-3"> </span>
+              <span className="text-lg">{hours % 10}</span>
+            </div>
+            <div className="timer-label">
+              <span className="label">Hours</span>
             </div>
           </div>
-          <div className="animate-fade-up mt-4  text-center">
-            <Button className="h-12 border bg-gradient-to-r from-green-400 to-blue-500 text-lg font-bold tracking-wide hover:opacity-70">
-              View Registration Fee
-            </Button>
+          <div className="timer-card max-w-lg mx-auto shadow-md rounded-lg overflow-hidden m-8 mr-8 font-bold p-2">
+            <div className="timer-value">
+              <span className="time">{Math.floor(minute / 10)}</span>
+              <span className="mr-3"> </span>
+              <span className="text-lg">{minute % 10}</span>
+            </div>
+            <div className="timer-label">
+              <span className="label">Mins</span>
+            </div>
           </div>
+
+          <div className="timer-card max-w-lg mx-auto shadow-md rounded-lg overflow-hidden m-8 mr-8 font-bold p-2">
+            <div className="timer-value">
+              <span className="text-lg">{Math.floor(second / 10)}</span>
+              <span className="mr-3"> </span>
+              <span className="text-lg">{second % 10}</span>
+            </div>
+            <div className="timer-label">
+              <span className="label">Sec</span>
+            </div>
+          </div>
+        </div>
+        <div className="animate-fade-up mt-2  text-center mb-2">
+          <Button className="h-12 border font-bold tracking-wide hover:opacity-70 ">
+            View Registration Fee
+          </Button>
         </div>
       </div>
     </section>
   );
 };
 
-export default EventTimer;
+export default EventTimer; 
+
+/*
+
+<div className="container grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-2">
+<div className="relative before:absolute before:inset-3 before:ml-5 before:-translate-x-px before:h-full before:w-1 before:bg-gradient-to-b before:from-transparent before:via-slate-600 before:to-transparent mt-8">
+  <div className="relative flex items-center">
+    <div className="flex items-center justify-center w-16 h-16 rounded-full border  bg-yellow-600 shadow">
+      <p> 09:00AM </p>
+    </div>
+    <p>
+      <ArrowRightIcon />
+    </p>
+
+    <div className=" p-8 rounded border shadow">
+      <div className="flex items-center justify-center mb-1">
+        <div className="font-bol">Event Start</div>
+      </div>
+      <div className="text-slate-500">
+        Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+        morbi blandit cursus risus.
+      </div>
+    </div>
+  </div>
+</div>
+
+<div className="relative before:absolute before:inset-3 before:ml-5 before:-translate-x-px before:h-full before:w-1 before:bg-gradient-to-b before:from-transparent before:via-slate-600 before:to-transparent mt-8">
+  <div className="relative flex items-center">
+    <div className="flex items-center justify-center w-16 h-16 rounded-full border  bg-yellow-600 shadow">
+      <p> 09:00AM </p>
+    </div>
+    <p>
+      <ArrowRightIcon />
+    </p>
+
+    <div className=" p-8 rounded border shadow">
+      <div className="flex items-center justify-center mb-1">
+        <div className="font-bol">Event Start</div>
+      </div>
+      <div className="text-slate-500">
+        Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+        morbi blandit cursus risus.
+      </div>
+    </div>
+  </div>
+</div>
+
+<div className="relative before:absolute before:inset-3 before:ml-5 before:-translate-x-px before:h-full before:w-1 before:bg-gradient-to-b before:from-transparent before:via-slate-600 before:to-transparent mt-8">
+  <div className="relative flex items-center">
+    <div className="flex items-center justify-center w-16 h-16 rounded-full border  bg-yellow-600 shadow">
+      <p> 09:00AM </p>
+    </div>
+    <p>
+      <ArrowRightIcon />
+    </p>
+
+    <div className=" p-8 rounded border shadow">
+      <div className="flex items-center justify-center mb-1">
+        <div className="font-bol">Event Start</div>
+      </div>
+      <div className="text-slate-500">
+        Pretium lectus quam id leo. Urna et pharetra aliquam vestibulum
+        morbi blandit cursus risus.
+      </div>
+    </div>
+  </div>
+</div>
+
+<EventTimer />
+
+<div className="max-w-lg mx-auto shadow-md rounded-lg text-black overflow-hidden m-8 mr-8 px-6">
+  <div className="flex justify-center mt-4 ">
+    <SubHeading content="Contact Person" />
+  </div>
+  <div className="container flex max-w-lg mx-auto shadow-md bg-gradient-to-r from-green-400 to-blue-500 rounded-lg sm:bg-slate-300 text-black overflow-hidden m-8">
+    <div className="mt-8">
+      <Avatar>
+        <AvatarImage
+          src="https://github.com/shadcn.png"
+          alt="@shadcn"
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+    <div className="m-4">
+      <h1>Name: Md Hazrat Ali</h1>
+      <h1>Designation: execative director</h1>
+      <h1>Phone Number: 018******66</h1>
+    </div>
+  </div>
+
+  <div className="container flex max-w-lg mx-auto bg-gradient-to-r from-pink-600 to-green-500 shadow-md rounded-lg sm:bg-slate-300 text-black overflow-hidden m-8">
+    <div className="mt-8">
+      <Avatar>
+        <AvatarImage
+          src="https://github.com/shadcn.png"
+          alt="@shadcn"
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+    <div className="m-4">
+      <h1>Name:</h1>
+      <h1>Designation:</h1>
+      <h1>Phone Number:</h1>
+    </div>
+  </div>
+
+  <div className="container flex max-w-lg mx-auto bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 shadow-md rounded-lg sm:bg-slate-300 text-black overflow-hidden m-8">
+    <div className="mt-8">
+      <Avatar>
+        <AvatarImage
+          src="https://github.com/shadcn.png"
+          alt="@shadcn"
+        />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+    <div className="m-4">
+      <h1>Name:</h1>
+      <h1>Designation:</h1>
+      <h1>Phone Number:</h1>
+    </div>
+  </div>
+</div>
+</div>  */
